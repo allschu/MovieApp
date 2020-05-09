fae5bdfbeb52e7f14d29ba93abda3883fadc1c8c
Allard Schuurmans <allard.schuurmans@4dotnet.nl>
2020-05-09 20:57:31 +0200

    Update detail.component.ts

--- src/movie/detail/detail.component.ts
+++ src/movie/detail/detail.component.ts
@@ -1,40 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../movie.service';
 import { MovieDetail } from '../models/movieDetail';
 import { MovieCredits } from '../models/movieCredits';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
-export class DetailComponent implements OnInit {
+export class DetailComponent implements OnInit, OnDestroy {
 
   constructor(private route: ActivatedRoute, private movieService: MovieService) { }
+ 
 
   public movie: MovieDetail = null;
   public movieCredits: MovieCredits[] = null;
   public movieRecommendations: MovieDetail[] = null;
-  
+
+  routeSubscription: Subscription;
+  movieSubscription: Subscription;
+  creditSubscription: Subscription;
+  relatedSubscription: Subscription;
+
+  // tslint:disable-next-line: variable-name
   movie_rating_average: string;
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      let movieId = params.get('movieId');
+   this.routeSubscription = this.route.paramMap.subscribe(params => {
+      const movieId = +params.get('movieId');
       // get movie
-      this.movieService.getMovie((parseInt(movieId))).subscribe((mov) => {
+      this.movieSubscription = this.movieService.getMovie(movieId).subscribe((mov) => {
         this.movie = mov;
-        this.movie_rating_average  = mov.vote_average.toFixed();
+        this.movie_rating_average = mov.vote_average.toFixed();
       });
       // only show first twelve charaters
-      this.movieService.getMovieCredits((parseInt(movieId))).subscribe((credits) => {
+      this.creditSubscription = this.movieService.getMovieCredits(movieId).subscribe((credits) => {
         this.movieCredits = credits.cast.slice(0, 12);
       });
       // show 4 recommendations
-      this.movieService.getMovieRecommendations((parseInt(movieId))).subscribe((recommendations) => {
+      this.relatedSubscription = this.movieService.getMovieRecommendations(movieId).subscribe((recommendations) => {
         this.movieRecommendations = recommendations.result.slice(0, 4);
       });
     });
+  }
+
+  ngOnDestroy(): void {
+    this.movieSubscription.unsubscribe();
+    this.creditSubscription.unsubscribe();
+    this.relatedSubscription.unsubscribe();
+    this.routeSubscription.unsubscribe();
   }
 }
